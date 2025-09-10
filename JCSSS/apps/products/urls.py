343f5af444c5fc3577f997c305d91e8015cd2db7
anjaliJT <anjali.johnnette@gmail.com>
2025-09-10 18:30:28 +0530

    Modified Product urls

--- JCSSS/apps/products/urls.py
+++ JCSSS/apps/products/urls.py
@@ -2,8 +2,8 @@ from django.urls import  path
 from .views import create_product, product_list,export_products,import_products
 
 urlpatterns = [
-    path("products/create",create_product,name="create_product"),
-    path("products", product_list, name="product_list"),
-    path("products/export",export_products,name="export_products"),
-    path("products/import",import_products,name="import_products")
+    path("", product_list, name="product_list"),
+    path("create",create_product,name="create_product"),
+    path("export",export_products,name="export_products"),
+    path("import",import_products,name="import_products")
 ]
