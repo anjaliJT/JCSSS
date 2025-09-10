a462b8f89727b5c921b6a55a7f72eb6c5ed61cd5
anjaliJT <anjali.johnnette@gmail.com>
2025-09-10 18:27:20 +0530

    added product url in main url

--- JCSSS/apps/urls.py
+++ JCSSS/apps/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include('apps.users.urls')),
-    path('complain/', include('apps.complain_form.urls'))
+    path('complain/', include('apps.complain_form.urls')),
+    path('product/',include('apps.products.urls'))
 ]
+
+
