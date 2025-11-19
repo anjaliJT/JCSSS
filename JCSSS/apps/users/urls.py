d1a56e3f7b74095ecfa7484c355c93b82c3b99b8
anjaliJT <anjali.johnnette@gmail.com>
2025-11-19 17:49:44 +0530

    updated urls for users' operations

--- JCSSS/apps/users/urls.py
+++ JCSSS/apps/users/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('logout/', logout_view, name="logout"),
     path("signup/",  SignupView.as_view(), name="signup"),
     path('profile/', profileView.as_view(),name="profile"),
-    path('users/admin/', UserManagementListView.as_view(), name='user_admin_list'),
-    path('users/admin/create/', CreateOEMUserView.as_view(), name='create_oem_user'),
-    path('users/admin/<int:pk>/edit/', EditOEMUserView.as_view(), name='edit_oem_user'),
+    path('users/', UserManagementListView.as_view(), name='user_list'),
+    path('users/create/', CreateOEMUserView.as_view(), name='create_oem_user'),
+    path('users/<int:pk>/edit/', EditOEMUserView.as_view(), name='edit_oem_user'),
     path('forgot-password/', send_otp, name='forgot-password-send-otp'),
     path('verify-otp/',  verify_otp, name= 'forgot-password-verify'),
 ]
