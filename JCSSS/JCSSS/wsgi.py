a917fab280b9fb8b6c3508b3f0dd69171fa2369a
anjaliJT <anjali.johnnette@gmail.com>
2025-12-27 15:01:18 +0530

    updated the config location

--- JCSSS/JCSSS/wsgi.py
+++ JCSSS/JCSSS/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JCSSS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JCSSS.config.prod')
 
 application = get_wsgi_application()
